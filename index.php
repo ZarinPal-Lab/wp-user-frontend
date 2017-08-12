9d712c9e4a253234fb1b01521e2e6463bb69b7da
yousefi1989 <alireza.yousefi98@gmail.com>
2017-08-12 11:18:31 +0430

    wp-user-frontend

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه زرین پال افزونه WP User Frontend
+Plugin URI: http://wpuf.ir/
+Description: درگاه زرین پال افزونه WP User Frontend
+Version: 1.1
+Author: گروه پشتیبانی  WP User Frontend
+Author URI: http://wpuf.ir/
+*/
+if ( !defined('ABSPATH') ) exit;
+include 'zarinpal.php';
